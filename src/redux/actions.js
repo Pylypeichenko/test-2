794e7c3c7996ad959bbbbd07d65d0eddcc55cb7a
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-11-19 12:13:19 +0200

    upd

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,13 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const addTask = createAction('tasks/addTask');
-
+import { createAction, nanoid } from '@reduxjs/toolkit';
+export const addTask = createAction('tasks/addTask', text => {
+  return {
+    payload: {
+      text,
+      id: nanoid(),
+      completed: false,
+    },
+  };
+});
 export const deleteTask = createAction('tasks/deleteTask');
 
 export const toggleCompleted = createAction('tasks/toggleCompleted');
